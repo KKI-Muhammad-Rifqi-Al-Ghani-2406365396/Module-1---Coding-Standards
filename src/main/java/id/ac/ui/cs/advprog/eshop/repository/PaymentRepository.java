ef25705e7f7483035690f36043ec5e1ecc5f8c29
alghani46 <alghani.gucci@gmail.com>
2026-03-10 18:07:00 +0700

    [GREEN] Proper Implementation of Payment Repo

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,14 +11,38 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        if (payment == null || payment.getPaymentId() == null) {
+            throw new IllegalArgumentException();
+        }
+        if (payment.getPaymentId() == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < paymentData.size(); i++) {
+            Payment savedPayment = paymentData.get(i);
+            if (savedPayment.getPaymentId() != null && savedPayment.getPaymentId().equals(payment.getPaymentId())) {
+                paymentData.set(i, payment);
+                return payment;
+            }
+        }
+
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String paymentId) {
+        if (paymentId == null) {
+            return null;
+        }
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getPaymentId() != null && savedPayment.getPaymentId().equals(paymentId)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public List<Payment> findAll() {
-        return null;
+        return new ArrayList<>(paymentData);
     }
 }
